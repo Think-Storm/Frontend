541cb90600b23bec2a0df3ff7ace5005a1795395
Vadim Kim <vadimkim@Vadims-MacBook-Air.local>
2025-03-31 23:04:25 +0900

    explore page basic layout

--- src/lib/constants/common.ts
+++ src/lib/constants/common.ts
@@ -30,3 +30,38 @@ export const OG_IMAGE = {
   height: 630,
   alt: "ThinkStorm",
 };
+
+export const FILTERS = [
+  {
+    placeholder: "Skills",
+    options: [
+      { label: "ReactJS", value: "react" },
+      { label: "NextJS", value: "next" },
+      { label: "TailwindCSS", value: "tailwind" },
+    ],
+  },
+  {
+    placeholder: "Field",
+    options: [
+      { label: "AI", value: "ai" },
+      { label: "Software Development", value: "software" },
+      { label: "iOS", value: "ios" },
+    ],
+  },
+  {
+    placeholder: "Purpose",
+    options: [
+      { label: "Self-development", value: "self" },
+      { label: "Hobby", value: "hobby" },
+      { label: "Portfolio", value: "portfolio" },
+    ],
+  },
+  {
+    placeholder: "Language",
+    options: [
+      { label: "Python", value: "python" },
+      { label: "JavaScript", value: "javascript" },
+      { label: "TypeScript", value: "typescript" },
+    ],
+  },
+];
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./reducers/user/userSlice";
 
 export const makeStore = () => {
   return configureStore({
-    reducer: {},
+    reducer: {
+      user: userSlice,
+    },
   });
 };
 
