3a949056f5eb932d312eddfd76d4706526d695d4
kimdahee0815 <billionairedahee@gmail.com>
2025-07-15 18:00:42 +0900

    feat: add framer-motion effect

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -3,7 +3,7 @@ import { Button, GradientButton } from "../ui/button";
 
 export default function Header() {
     return (
-        <div className="flex justify-between items-center p-4 bg-white text-black mt-1">
+        <div className="sticky top-0 z-100 flex justify-between items-center p-4 bg-white text-black mt-1">
             <div className="flex items-center gap-[4px] ml-6">
                 <Image src="/images/logo-gradient.png" alt="Logo" width={45} height={25} />
                 <span className="text-4xl leading-[30px] font-semibold h-[30px] flex items-center">ThinkStorm</span>
