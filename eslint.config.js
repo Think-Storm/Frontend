0ac4012d7cd6911bc4c171f30a5c1aa3817a05c9
kimdahee0815 <billionairedahee@gmail.com>
2025-09-04 23:15:01 +0900

    fix: eslint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,43 @@
 import nextPlugin from "@next/eslint-plugin-next";
 import storybookPlugin from "eslint-plugin-storybook";
+import typescriptParser from "@typescript-eslint/parser";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**"],
+  },
+  {
     files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     plugins: {
       "@next/next": nextPlugin,
+      "@typescript-eslint": typescriptPlugin,
     },
     rules: {
       ...nextPlugin.configs["core-web-vitals"].rules,
+      ...typescriptPlugin.configs.recommended.rules,
     },
   },
   {
     files: ["**/*.stories.{js,jsx,ts,tsx}", ".storybook/**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: typescriptParser,
+    },
     plugins: {
       storybook: storybookPlugin,
     },
     rules: {
       ...storybookPlugin.configs.recommended.rules,
     },
-  },
-  {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    rules: {},
   },
 ];
